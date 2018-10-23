5def2b90b5cfceb6f1ee2754a5a75abefc0f79e0
Jaswinder Bola <jassisaini96.jb@gmail.com>
2018-10-23 16:46:51 -0400

    refactored todoController

--- src/controllers/todoControllers.js
+++ src/controllers/todoControllers.js
@@ -1,33 +1,21 @@
 import mongoose from "mongoose";
 import { TodoSchema } from "../models/todoModels";
 
-const Todo = mongoose.model("Todo", TodoSchema)
+
+const handleAction = (res) => (err,todo) => 
+  err ? res.send(err) : res.json(todo) ;
+
+const Todo = mongoose.model("Todo", TodoSchema);
 
 export const addNewTodo = (req, res) => {
   let newTodo = new Todo(req.body);
-
-  newTodo.save((err, todo) => {
-    if(err){
-      res.send(err);
-    }
-    res.json(todo);
-  })
-};
+  newTodo.save( handleAction(res) );
+}  
 
 export const getTodos = (req, res) => {
-  Todo.find({} , (err, todo) => {
-      if(err){
-        res.send(err);
-      }
-      res.json(todo);
-  });
+  Todo.find({} , handleAction(res) ); 
 };
 
 export const getTodoWithID = (req, res) =>{
-  Todo.findById(req.params.todoId, (err, todo) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json(todo);
-  });
+  Todo.findById(req.params.todoId, handleAction(res) ); 
 }
